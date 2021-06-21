42abfe873697e62e6b5c3030dfab88085ee5964a
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2021-06-21 21:09:09 +0300

    Extracted common KorGE init code into abstract class

---
+++ src/commonMain/kotlin/io/baba4/syebatal/client/KTreeScene.kt
@@ -0,0 +1,20 @@
+package io.baba4.syebatal.client
+
+import com.soywiz.korge.scene.Scene
+import com.soywiz.korge.view.Container
+import com.soywiz.korge.view.View
+import com.soywiz.korge.view.ktree.readKTree
+import com.soywiz.korio.file.std.resourcesVfs
+
+
+abstract class KTreeScene(
+    private val kTreeName: String
+) : Scene() {
+    abstract suspend fun init(kTree: View)
+
+    override suspend fun Container.sceneInit() {
+        val tree = resourcesVfs["$kTreeName.ktree"].readKTree(views)
+        addChild(tree)
+        init(tree)
+    }
+}
--- src/commonMain/kotlin/io/baba4/syebatal/client/MainMenuScene.kt
+++ src/commonMain/kotlin/io/baba4/syebatal/client/MainMenuScene.kt
@@ -1,19 +1,13 @@
 package io.baba4.syebatal.client
 
-import com.soywiz.korge.scene.Scene
-import com.soywiz.korge.view.Container
+import com.soywiz.korge.view.View
 import com.soywiz.korge.view.get
-import com.soywiz.korge.view.ktree.readKTree
 import com.soywiz.korge.view.onClick
-import com.soywiz.korio.file.std.resourcesVfs
 
 
-class MainMenuScene : Scene() {
-    override suspend fun Container.sceneInit() {
-        val tree = resourcesVfs["main_menu.ktree"].readKTree(views)
-        addChild(tree)
-
-        val button = tree["start_game"]
+class MainMenuScene : KTreeScene(kTreeName = "main_menu") {
+    override suspend fun init(kTree: View) {
+        val button = kTree["start_game"]
         button.onClick {
             println("-> Hello World!")
         }
